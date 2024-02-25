934ac442ac124391cd1bf58e604daf1c3fb57aab
Evgeni.ok14 <evgeni.ok@mail.ru>
2024-02-25 19:54:24 +0300

    new changes dz_9

--- DZ_8/aspect_dz_8/src/main/java/com/example/aspect_dz_8/models/Person.java
+++ DZ_8/aspect_dz_8/src/main/java/com/example/aspect_dz_8/models/Person.java
@@ -21,19 +21,23 @@ public class Person
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "name")
     @NotEmpty(message = "Поле не должно быть пустым! ")
     @Size(min = 2, max = 30, message = "имя должно содержать от 2 до 30 символов! ")
     private String name;
 
+    @Column(name = "surname")
     @NotEmpty(message = "Поле не должно быть пустым! ")
     private String surname;
 
-
+    @Column(name = "age")
     @NotNull(message = "Графу возраст необходмо заполнить! ") // для типа int применяеться @NotNull
     @Max(value = 300, message = "Возраст не может быть более 300 лет!")
     @Min(value = 0, message = "возраст должен быть больше 0! ")
     private int age;
 
+    @Column(name = "email")
     @NotEmpty(message = "Поле не должно быть пустым! ")
     @Email(message = "почта должна соответствовать требованиям email! ")
     private String email;
